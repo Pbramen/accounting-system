3913ee4ff84227499ce37cb9816bd5644906b8de
Peter Banh <peterbanh@Lams-MacBook-Pro.local>
2025-01-21 13:10:56 -0600

    re-added entity field for faster queries

--- vendor/django-ledger/django_ledger/models/category.py
+++ vendor/django-ledger/django_ledger/models/category.py
@@ -1,5 +1,12 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from typing import Union
+from uuid import UUID
+from django_ledger.models.entity import EntityModel
+from django_ledger.settings import DJANGO_LEDGER_ENTITY_MODEL
+
+print(DJANGO_LEDGER_ENTITY_MODEL)
+# from django_ledger.models.entity import EntityModel
 
 class CategoryModelQuerySet(models.QuerySet):
     ...
@@ -7,11 +14,18 @@ class CategoryModelQuerySet(models.QuerySet):
 class CategoryModelManager(models.Manager):
     def get_queryset(self) -> CategoryModelQuerySet:
         return CategoryModelQuerySet(self.model, using=self._db)
-        
 
+    def for_entity(self, entity_slug: Union[str, UUID, EntityModel]):
+        qs = self.get_queryset()
+
+        if isinstance(entity_slug, EntityModel):
+            return qs.filter(entity=entity_slug)
+        elif isinstance(entity_slug, UUID):
+            return qs.filter(entity_id=entity_slug)
+        return qs.filter(entity__slug__exact=entity_slug)
+            
 class CategoryModelAbstract(models.Model):
     '''
-
         An abstract class reprenting the set of catagories belogning to a specifc entity
             * note same named catagories may have different meaning among different entities
             * catagories are made for aggregating transactions (See models/transactions.py)
@@ -21,8 +35,7 @@ class CategoryModelAbstract(models.Model):
         
         - name: label of the category. Used in queries to select/group transactoins
         - description: a short optional value describing the category
-        - entity: entity this category belongs to
-            accessible to any user that has access to this entity
+        - entity: entity the category belongs to
     '''
 
     class Meta:
@@ -31,6 +44,9 @@ class CategoryModelAbstract(models.Model):
 
     name = models.CharField(max_length=32, null=False)
     description = models.CharField(max_length=128, null=True)
+    entity = models.ForeignKey('django_ledger.EntityModel',
+                            on_delete=models.CASCADE,
+                            verbose_name=_('Category Entity'))
     
     objects = CategoryModelManager()
 
